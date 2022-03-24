0ec7c1a068fb333e76172588abecff492d074bc7
Álvaro Teixeira de Araújo <dralvphd2@gmail.com>
2022-03-23 22:01:31 -0300

    Adicionando quantificadores à Regex

--- extrator_cep.py
+++ extrator_cep.py
@@ -2,7 +2,7 @@ import re #módulo do python para expressões regulares
 
 endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440120"
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 
 busca = padrao.search(endereco) #search é um método do objeto pattern criado pelo compile devolvendo um objeto Match
 
